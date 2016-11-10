224111c71ffb2f50b0c0e5f6ab55ec5f26c5710a
Zhuode Liu <zhuode93@cs.utexas.edu>
2016-11-09 20:56:47 -0600

    update custom_vgg19.py

--- custom_vgg19.py
+++ custom_vgg19.py
@@ -4,17 +4,15 @@ import tensorflow as tf
 
 import numpy as np
 import time
-from tensorflow_vgg import vgg19
+from tensorflow_vgg import vgg19_factory
 
 VGG_MEAN = [103.939, 116.779, 123.68]
 
 
-class Vgg19(vgg19.Vgg19):
+class Vgg19Factory(vgg19_factory.Vgg19Factory):
     # Input should be an rgb image [batch, height, width, 3]
     # values scaled [0, 1]
     def build(self, rgb, train=False):
-        start_time = time.time()
-        print "build model started"
         rgb_scaled = rgb * 255.0
 
         # Convert RGB to BGR
@@ -25,31 +23,33 @@ class Vgg19(vgg19.Vgg19):
             red - VGG_MEAN[2],
         ])
 
-        self.conv1_1 = self.conv_layer(bgr, "conv1_1")
-        self.conv1_2 = self.conv_layer(self.conv1_1, "conv1_2")
-        self.pool1 = self.avg_pool(self.conv1_2, 'pool1')
+        class VGG19Object: pass
+        vgg19 = VGG19Object()
 
-        self.conv2_1 = self.conv_layer(self.pool1, "conv2_1")
-        self.conv2_2 = self.conv_layer(self.conv2_1, "conv2_2")
-        self.pool2 = self.avg_pool(self.conv2_2, 'pool2')
+        vgg19.conv1_1 = self.conv_layer(bgr, "conv1_1")
+        vgg19.conv1_2 = self.conv_layer(vgg19.conv1_1, "conv1_2")
+        vgg19.pool1 = self.avg_pool(vgg19.conv1_2, 'pool1')
 
-        self.conv3_1 = self.conv_layer(self.pool2, "conv3_1")
-        self.conv3_2 = self.conv_layer(self.conv3_1, "conv3_2")
-        self.conv3_3 = self.conv_layer(self.conv3_2, "conv3_3")
-        self.conv3_4 = self.conv_layer(self.conv3_3, "conv3_4")
-        self.pool3 = self.avg_pool(self.conv3_4, 'pool3')
+        vgg19.conv2_1 = self.conv_layer(vgg19.pool1, "conv2_1")
+        vgg19.conv2_2 = self.conv_layer(vgg19.conv2_1, "conv2_2")
+        vgg19.pool2 = self.avg_pool(vgg19.conv2_2, 'pool2')
 
-        self.conv4_1 = self.conv_layer(self.pool3, "conv4_1")
-        self.conv4_2 = self.conv_layer(self.conv4_1, "conv4_2")
-        self.conv4_3 = self.conv_layer(self.conv4_2, "conv4_3")
-        self.conv4_4 = self.conv_layer(self.conv4_3, "conv4_4")
-        self.pool4 = self.avg_pool(self.conv4_4, 'pool4')
+        vgg19.conv3_1 = self.conv_layer(vgg19.pool2, "conv3_1")
+        vgg19.conv3_2 = self.conv_layer(vgg19.conv3_1, "conv3_2")
+        vgg19.conv3_3 = self.conv_layer(vgg19.conv3_2, "conv3_3")
+        vgg19.conv3_4 = self.conv_layer(vgg19.conv3_3, "conv3_4")
+        vgg19.pool3 = self.avg_pool(vgg19.conv3_4, 'pool3')
 
-        self.conv5_1 = self.conv_layer(self.pool4, "conv5_1")
-        self.conv5_2 = self.conv_layer(self.conv5_1, "conv5_2")
-        self.conv5_3 = self.conv_layer(self.conv5_2, "conv5_3")
-        self.conv5_4 = self.conv_layer(self.conv5_3, "conv5_4")
-        self.pool5 = self.avg_pool(self.conv5_4, 'pool5')
+        vgg19.conv4_1 = self.conv_layer(vgg19.pool3, "conv4_1")
+        vgg19.conv4_2 = self.conv_layer(vgg19.conv4_1, "conv4_2")
+        vgg19.conv4_3 = self.conv_layer(vgg19.conv4_2, "conv4_3")
+        vgg19.conv4_4 = self.conv_layer(vgg19.conv4_3, "conv4_4")
+        vgg19.pool4 = self.avg_pool(vgg19.conv4_4, 'pool4')
 
-        self.data_dict = None
-        print "build model finished: %ds" % (time.time() - start_time)
+        vgg19.conv5_1 = self.conv_layer(vgg19.pool4, "conv5_1")
+        vgg19.conv5_2 = self.conv_layer(vgg19.conv5_1, "conv5_2")
+        vgg19.conv5_3 = self.conv_layer(vgg19.conv5_2, "conv5_3")
+        vgg19.conv5_4 = self.conv_layer(vgg19.conv5_3, "conv5_4")
+        vgg19.pool5 = self.avg_pool(vgg19.conv5_4, 'pool5')
+
+        return vgg19
